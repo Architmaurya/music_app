1286fc66e51663afe52d332717a83016b2b79664
Architmaurya <architmaurya7054@gmail.com>
2025-08-20 20:41:25 +0530

    <feat>fixing the navigation

--- music_app/app/navigation/AppNavigator.js
+++ music_app/app/navigation/AppNavigator.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from '../screen/HomeScreen';
-import DetailsScreen from '../screen/DetailsScreen';
-import SearchScreen from '../screen/SearchScreen';
-import ProfileScreen from '../screen/ProfileScreen';
-import BottomMenuBar from '../components/Common/BottomMenuBar';
-import WelcomeScreen from '../screen/WelcomeScreen';
-import RegistionScreen from '../screen/RegistionScreen';
-import EnterOtpScreen from '../screen/EnterOtpScreen';
+import HomeScreen from "../screen/HomeScreen";
+import DetailsScreen from "../screen/DetailsScreen";
+import SearchScreen from "../screen/SearchScreen";
+import ProfileScreen from "../screen/ProfileScreen";
+import BottomMenuBar from "../components/Common/BottomMenuBar";
+import WelcomeScreen from "../screen/WelcomeScreen";
+import RegistionScreen from "../screen/RegistionScreen";
+import EnterOtpScreen from "../screen/EnterOtpScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,7 +19,7 @@ const Tab = createBottomTabNavigator();
 function HomeStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="HomeMain" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
   );
@@ -31,7 +32,7 @@ function MainTabs() {
       screenOptions={{ headerShown: false }}
       tabBar={(props) => <BottomMenuBar {...props} />}
     >
-      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="HomeTab" component={HomeStack} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
@@ -41,11 +42,16 @@ function MainTabs() {
 // Root stack: Welcome first, then tabs
 export default function AppNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Registion" component={RegistionScreen} />
-      <Stack.Screen name="EnterOtp" component={EnterOtpScreen} />
-      <Stack.Screen name="HomeMain" component={MainTabs} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Registion" component={RegistionScreen} />
+        <Stack.Screen name="EnterOtp" component={EnterOtpScreen} />
+        <Stack.Screen name="MainTabs" component={MainTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
